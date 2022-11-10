c0e1a25fe532935a129f24ff62d3869bfacc37f2
Tuxinal <24763016+tuxinal@users.noreply.github.com>
2022-11-10 08:48:51 +0330

    Initial Commit

---
+++ src/main/java/xyz/tuxinal/BootstrapMain.java
@@ -0,0 +1,17 @@
+package xyz.tuxinal;
+
+import link.infra.packwiz.installer.Main;
+
+public class BootstrapMain {
+    public static void main(String[] args) {
+        String entrypoint = System.getProperty("entrypoint");
+        String packurl = System.getProperty("packUrl");
+        try {
+            Class<?> entrypointClass = Class.forName(entrypoint);
+            Main.main(new String[] { packurl });
+            entrypointClass.getDeclaredMethod("main", String[].class).invoke(null, (Object)args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
